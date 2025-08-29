4c518f1f2fe62be97a9d372f25006c6de67c57ac
pebolozi <pebolozi212@gmail.com>
2025-08-29 10:06:11 +0900

    파일 경로 수정

--- js/assets.js
+++ js/assets.js
@@ -6,9 +6,7 @@
     { rel:"preconnect", href:"https://fonts.gstatic.com", crossOrigin:"anonymous" },
     { rel:"stylesheet", href:"https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700&family=Noto+Sans+KR:wght@100..900&display=swap" },
     { rel:"stylesheet", href:"https://use.typekit.net/nen3mlm.css" },
-    { rel:"stylesheet", href:"https://fonts.googleapis.com/icon?family=Material+Icons" },
-    { rel:"stylesheet", href:"../css/common.css" },
-    { rel:"stylesheet", href:"../css/main.css" }
+    { rel:"stylesheet", href:"https://fonts.googleapis.com/icon?family=Material+Icons" }
   ];
   links.forEach(attr => {
     const link = document.createElement("link");
